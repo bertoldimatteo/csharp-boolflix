154ebc7298ffced6e835befd88f6850c970bcdbb
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-10-12 12:44:03 +0200

    creati model con relazioni + aggiunto db connection e aggionri model a db

---
+++ csharp-boolflix/Migrations/BoolflixContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace csharp_boolflix.Migrations
+{
+    public class BoolflixContext : DbContext 
+    {
+        public DbSet<Film>Films { get; set; }
+        public DbSet<TvSerie> TvSerie { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=boolflix-db;Integrated Security=True");
+        }
+    }
+}
---
+++ csharp-boolflix/Models/Model.cs
@@ -0,0 +1,31 @@
+﻿using csharp_boolflix.Models;
+
+namespace csharp_boolflix.Models
+{
+    public abstract class Model
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Compatible { get; set; }
+        public int Age { get; set; }
+        public string Image { get; set; }
+
+        public int? TvSerieId { get; set; }
+        public TvSerie? TvSerie { get; set; }
+
+        public int? FilmId { get; set; }
+        public Film? Film { get; set; }
+    }
+}
+
+public class TvSerie : Model
+{
+    public int Seasons { get; set; }
+    public Model Model { get; set; }
+}
+
+public class Film : Model
+{
+    public int Duration { get; set; }
+    public Model Model { get; set; }
+}
