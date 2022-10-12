7776acadfea66ccc74c3e7ff875876599d5a36aa
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-10-12 11:33:10 +0200

    Add project files.

---
+++ csharp-boolflix/Views/Boolflix/InitialSelect.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace csharp_boolflix.Views.Boolflix
+{
+    public class InitialSelectModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ csharp-boolflix/Views/Boolflix/home.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace csharp_boolflix.Views.Boolflix
+{
+    public class homeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
